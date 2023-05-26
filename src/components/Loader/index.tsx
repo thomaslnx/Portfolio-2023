2121bc15e26abe758d135fa6b84b5440e0ad1502
thomaslnx <marcos.msilva10@gmail.com>
2023-05-25 21:00:34 -0300

    - Fixed the window reference to pageScroll function.

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,5 +1,3 @@
-import React, { MutableRefObject, RefObject } from 'react'
-
 import { Preloader, Loading } from './styles'
 
 interface LoaderProps {
@@ -9,12 +7,10 @@ interface LoaderProps {
 
 const Loader: React.FC<LoaderProps> = (
   { visibility, display }: LoaderProps,
-  // ref: RefObject<MutableRefObject<null>>
 ): JSX.Element => (
-  <Preloader /* ref={ref} */ id="preloader" display={display} visibility={visibility}>
+  <Preloader id="preloader" display={display} visibility={visibility}>
     <Loading id="loader" />
   </Preloader>
 )
 
-// export default forwardRef<React.MutableRefObject<null>, LoaderProps> (Loader)
 export default Loader;
