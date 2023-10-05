a8806055d879b9660ccd8c822a0c44219eca8c55
thomaslnx <marcos.msilva10@gmail.com>
2023-10-05 11:10:26 -0300

    Changed the picture from About section; Added golang icon to about text; Changed the way that images were imported; Improve the about text; Added my personal CV link from drive to the download button; Improve my past job activities description.

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image'
+import Marcos from '../../../public/images/Marcos.png'
+import Gopher from '../../../public/images/go.svg'
 import { Container } from './styles'
 
 const About: React.FC = (): JSX.Element => (
@@ -6,8 +8,8 @@ const About: React.FC = (): JSX.Element => (
     <div className="row about-info wide" data-animate-block>
       <div className="column lg-6 md-12 about-info__pic-block">
         <Image
-          src="/images/about.jpg"
-          alt="about"
+          src={Marcos}
+          alt="Marcos"
           width={700}
           height={868}
           className="about-info__pic"
@@ -21,19 +23,27 @@ const About: React.FC = (): JSX.Element => (
             About
           </h2>
           <p className="attention-getter" data-animate-el>
-            Hi, Marcos Moura here! 👋 A little bit about myself: I`&apos;`m married to
-            the most wonderful woman ever and I have two little beautiful
-            princesses. I`&apos;`m a Jr Javascript Full Stack developer with about 3
-            years of experience, recently working mainly with ReactJS and NodeJS
-            but I have some experience with React Native too. I`&apos;`m always keeping
-            in move, learning something new that is so new and exciting to me
-            and which makes me more productive and valuable as a professional
-            developer. I love coding because is there no boring routine from
-            doing daily the same thing and as proof virtually every single day I
-            learn or do something completely new to me.
+            Hi, Marcos Moura here! 👋 I&apos;m a FullStack Software Engineer
+            working mainly with JavaScript and its associated most famous
+            frameworks such as ReactJS, NextJS, NodeJS, Typescript, TailwindCSS,
+            Styled-Components, REST, and GraphQL APIs.
+            <br/> <br/>
+            I&apos;m always keeping
+            in moving, learning new things, like Golang &nbsp;
+            <Image
+              src={Gopher}
+              alt="Marcos"
+              width={30}
+              height={30}
+              className="about-info__pic"
+              data-animate-el
+            />, and deep and advanced JavaScript concepts, such as NodeJS Streams,
+            benchmarking, memory leaks, long-running processes from NodeJS, and so on.
+            I&apos;ll start putting all this into practical projects, like this one to
+            prove myself about what I&apos;m learning. So, stay tuned.
           </p>
           <a
-            href="https://drive.google.com/file/d/1XJX-AsbRF9Tc9Qn994Ek8WiMyxYVDwsv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1UP6JuLPAX0hBaN1Rdnr6qPD7M2veH_G1/view?usp=drive_link"
             target="_blank"
             className="btn btn--medium u-fullwidth"
             data-animate-el
@@ -57,10 +67,13 @@ const About: React.FC = (): JSX.Element => (
           <li>NextJS</li>
           <li>TypeScript</li>
           <li>GraphQL</li>
+          <li>Rest API</li>
           <li>MongoDB</li>
           <li>PostgreSQL</li>
           <li>Material UI</li>
           <li>Styled-Components</li>
+          <li>Docker</li>
+          <li>Tailwind CSS</li>
         </ul>
       </div>
     </div>
@@ -82,13 +95,13 @@ const About: React.FC = (): JSX.Element => (
             </div>
             <div className="timeline__desc">
               <p>
-              As a full-stack software engineer, I was responsible to automate some
-              processes like the sending of push notifications through our mobile app
-              every time one new job opportunity was included on our job board to all
+              As a full-stack software engineer, I was responsible for automating some
+              processes, like the sending of push notifications through our mobile app
+              every time one new job opportunity was included in our job board to all
               photographers that fit the job requirements. This automation makes the
               inner team save time and reach our target audience at once. For this task,
               I used technologies like MongoDB, PostgreSQL, GraphQL, React, NodeJS,
-              Typescript, Amazon SNS Service, and others.
+              Typescript, Amazon SNS Service, Amazon SQS Service, and others.
               </p>
             </div>
           </div>
@@ -96,15 +109,19 @@ const About: React.FC = (): JSX.Element => (
             <div className="timeline__bullet" />
             <div className="timeline__header">
               <h4 className="timeline__title">Tog Design</h4>
-              <h5 className="timeline__meta">Jr Full Stack Developer</h5>
+              <h5 className="timeline__meta">Jr FrontEnd Developer</h5>
               <p className="timeline__timeframe">August 2021 - April 2022</p>
             </div>
             <div className="timeline__desc">
               <p>
-                Helping Tog`&apos;`s team to deliver high-quality software to your
-                customers as Jr front end developer using mainly JavaScript,
-                React, and Typescript, and at the same time that I get improving
-                my own hard and soft skills.
+                Helping Tog&apos;s team to deliver high-quality software to your customers
+                as Jr front end developer using mainly JavaScript, React, and Typescript.
+                During my time at the company, I had the opportunity to work on two projects.
+                At first, I was implementing the entire layout from the design team to an inner
+                project. For this, I used React, Typescript, and Styled-Components. For the
+                second project, I worked as an outsource with a partner&apos;s team, where we
+                were migrating the legacy frontend system to React using Typescript using the
+                client&apos;s style guide.
               </p>
             </div>
           </div>
@@ -116,19 +133,18 @@ const About: React.FC = (): JSX.Element => (
                 Universidade Federal do Tocantins - UFT
               </h4>
               <h5 className="timeline__meta">
-                Information Technology Technician
+                Infrastructure Manager
               </h5>
               <p className="timeline__timeframe">January 2010 - August 2021</p>
             </div>
             <div className="timeline__desc">
               <p>
-                In this role, I was acting as a technology technician been one
-                of responsible for maintaining the health of all IT
+                In this role, I was acting as a infrastructure Manager,
+                responsible for maintaining the health of all IT
                 infrastructure, networking, and VoIP and deploying new features
                 to maintain all services. I have implemented some scripts using
-                shell script and python to automate some repetitive tasks. I was
-                worked too with services virtualization using VMWare solutions
-                like VSphere.
+                shell script and python to automate some repetitive tasks on the server
+                side. I worked too with virtualization solutions like VSphere from VMWare.
               </p>
             </div>
           </div>
